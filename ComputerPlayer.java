ab1aef03d54f15ed55aa79d305c920b07e8e8cd9
msl2141 <michaellanclos157@gmail.com>
2019-04-10 10:16:46 -0500

    added some logic where it places the disc

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -17,7 +17,7 @@ public class ComputerPlayer implements Player {
   
   
   public int selectColumn() {
-    
+    Disc[][] board = Board.getInstance().getBoard();
     /*for now, this simply drops a disc at a random column
     *check for the next move by the player being a connect four 4 with a connect 4 method. 
     *would be a simple as crtl c ctrl v the connect 4 method - 1 to their checks
@@ -25,8 +25,20 @@ public class ComputerPlayer implements Player {
     *if false drop the disc on a dics that is mine or on the side of mine
     *if their is none, drop a disc at a random location
     */
-    Board.getInstance().getBoard();
-    return (int)Math.random()*6;
+    //attempt to check for 3 in a row, any direction
+    for (int r = 0; r < 6; r++){
+      for (int c = 0; c < 7; c++){
+        //if(board[i][a].getColor())
+      }
+    }
+    
+    for (int i = 0; i < 6; i++){//if no 3 in a row, puts a disc on one that is already plaed and is my color
+      for (int a = 0; a < 7; a++){
+        if(board[i][a].isYellow() && i != 6)
+          return a;
+      }
+    }
+    return (int)Math.random()*6;//if all else fails, places disc in a random place.
     
   }
 }
